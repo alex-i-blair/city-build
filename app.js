c32ef77d2cfc0fd32273e1a72bfe8a6d5d0d147f
Alex Blair <bclan11@gmail.com>
2021-12-02 16:17:43 -0800

    skyline working

--- app.js
+++ app.js
@@ -6,3 +6,29 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+  
+const skylineImageEl = document.getElementById('skyline-image');
+const castleImageEl = document.getElementById('castle-image');
+const waterfrontImageEl = document.getElementById('waterfront-image');
+const skylineSelectEl = document.getElementById('skyline-dropdown');
+const castleSelectEl = document.getElementById('castle-dropdown');
+const waterfrontSelectEl = document.getElementById('waterfront-dropdown');
+const sloganInputEl = document.getElementById('slogan-input');
+const sloganButtonEl = document.getElementById('slogan-button');
+const sloganCountEl = document.getElementById('count');
+
+skylineSelectEl.addEventListener('change', () => {
+    let city = skylineSelectEl.value;
+    console.log(city);
+    skylineImageEl.src = `./assets/${city}-skyline.jpeg`;
+});
+castleSelectEl.addEventListener('change', () => {
+    let castle = castleSelectEl.value;
+    castleImageEl.src = `./assets/${castle}-castle`;
+});
+waterfrontSelectEl.addEventListener('change', () => {
+
+});
+sloganButtonEl.addEventListener('click', () => {
+
+});
