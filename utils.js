b71eccb4827c867997cce5eed43805939e1f3015
Alex Blair <bclan11@gmail.com>
2021-12-03 12:43:11 -0800

    import function

---
+++ utils.js
@@ -0,0 +1,3 @@
+export function makeCountString(waterCount, skylineCount, castleCount) {
+    return `You've changed the waterfront ${waterCount} times, the skyline ${skylineCount} times, and the castle ${castleCount} times.`;
+}
